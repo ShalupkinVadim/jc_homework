4e3894b4f0478a6cf1a2152177078408d35336cf
ShalupkinVadim <shalupkin.va@gmail.com>
2022-03-21 14:24:28 +0300

    example17

---
+++ src/net/example17/Main.java
@@ -0,0 +1,9 @@
+package net.example17;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 20023143;
+        String str = String.format("%,d",number);
+        System.out.println(str);
+    }
+}
