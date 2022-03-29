6de23bbfe92acbaf9cb4762ab892fb12b1d30c9d
ShalupkinVadim <shalupkin.va@gmail.com>
2022-03-29 15:39:19 +0300

    example23

---
+++ src/net/example23/Main.java
@@ -0,0 +1,12 @@
+package net.example23;
+
+import net.common.Array;
+
+public class Main {
+    public static void main(String[] args) {
+        Array array = new Array();
+        array.setSizeButterfly(11);
+        array.fillArrayButterfly();
+        array.drawButterfly();
+    }
+}
